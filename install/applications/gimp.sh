477e38161786617d96b6097dae3c28db35db5d73
jorge <kalmbach@gmail.com>
2024-08-05 16:48:42 -0300

    Assume yes in flatpak installs

--- install/applications/gimp.sh
+++ install/applications/gimp.sh
@@ -1,6 +1,6 @@
 if ! flatpak run org.gimp.GIMP --version &> /dev/null; then
   gum log --level info "installing gimp..."
-  flatpak install flathub org.gimp.GIMP
+  flatpak install -y flathub org.gimp.GIMP
 else
   gum log --level info "found gimp $(flatpak run org.gimp.GIMP --version | awk '{print $NF}')"
 fi
